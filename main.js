86caeb1cc3745d9090a4c2c7e219e7841b54b478
SolihovAbdulloh <abdullasolikh@mail.com>
2024-11-01 08:08:39 +0500

    sad

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+// const Btn_uslugi = document.getElementById('uslugi') as HTMLElement
+// Btn_uslugi.addEventListener('click',()=>{
+//     let p = document.createElement('p')
+// })
+const Bars = document.querySelector("#bars");
+const navbar = document.querySelector(".ul");
+Bars.addEventListener("click", () => { navbar.classList.toggle('active'); });
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,17 @@
+// const Btn_uslugi = document.getElementById('uslugi') as HTMLElement
+
+// Btn_uslugi.addEventListener('click',()=>{
+    
+//     let p = document.createElement('p')
+
+    
+    
+// })
+
+const Bars = document.querySelector("#bars") as HTMLElement;
+const navbar = document.querySelector(".ul") as HTMLElement;
+
+Bars.addEventListener("click", () => {navbar.classList.toggle('active')})
+    
+  
+
